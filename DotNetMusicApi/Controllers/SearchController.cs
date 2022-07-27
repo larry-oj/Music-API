0929cb1a66ea72b295748fc3102456de60e37a1b
Larry <66905431+larry-oj@users.noreply.github.com>
2022-07-27 21:52:02 +0300

    add search controller base

---
+++ DotNetMusicApi/Controllers/SearchController.cs
@@ -0,0 +1,27 @@
+﻿using DotNetMusicApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetMusicApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SearchController : ControllerBase
+{
+    private readonly ILogger<SearchController> _logger;
+    private readonly ISpotifyService _spotifyService;
+    private readonly IYouTubeService _youTubeService;
+    private readonly IConfiguration _configuration;
+
+    public SearchController(ILogger<SearchController> logger, 
+        ISpotifyService spotifyService, 
+        IYouTubeService youTubeService, 
+        IConfiguration configuration)
+    {
+        _logger = logger;
+        _spotifyService = spotifyService;
+        _youTubeService = youTubeService;
+        _configuration = configuration;
+    }
+    
+    // ...
+}
