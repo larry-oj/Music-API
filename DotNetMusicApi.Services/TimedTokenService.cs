42436315d7df02bbb0c49b0da19fc66d48593dc9
Larry <66905431+larry-oj@users.noreply.github.com>
2022-07-27 16:56:06 +0300

    add spotify token refresh service

---
+++ DotNetMusicApi.Services/TimedTokenService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace DotNetMusicApi.Services;
+
+public class TimedTokenService : IHostedService, IDisposable
+{
+    private readonly ILogger<TimedTokenService> _logger;
+    private readonly IHttpClientFactory _clientFactory;
+    private readonly IConfiguration _configuration;
+    private Timer _timer;
+    
+    public TimedTokenService(ILogger<TimedTokenService> logger,
+        IHttpClientFactory clientFactory, 
+        IConfiguration configuration)
+    {
+        _logger = logger;
+        _clientFactory = clientFactory;
+        _configuration = configuration;
+    }
+    
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Timed token service is running");
+
+        _timer = new Timer(Generate, null, TimeSpan.Zero, TimeSpan.FromMinutes(45));
+
+        return Task.CompletedTask;
+    }
+
+    private void Generate(object? state)
+    {
+        // ...
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Timed token service is stopping");
+
+        _timer?.Change(Timeout.Infinite, 0);
+
+        return Task.CompletedTask;
+    }
+
+    public void Dispose()
+    {
+        _timer.Dispose();
+    }
+}
